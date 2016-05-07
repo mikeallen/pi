c1716121a96bbd78fef0b826b06bacb7da9cd6b5
Mike Allen <mike.p.allen@gmail.com>
2016-05-07 09:54:59 -0700

    added etherdreamtest and X circle demo

--- X/build.sh
+++ X/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #gcc hix.c -L/usr/X11R6/lib -lX11 -o hix
 gcc hix.c -L/usr/X11R6/lib -L/usr/X11/lib -I/usr/X11/include -lX11 -o hix
+gcc hix_circle.c -L/usr/X11R6/lib -L/usr/X11/lib -I/usr/X11/include -lX11 -o hix_circle
